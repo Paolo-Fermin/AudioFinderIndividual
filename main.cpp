0d68d72ee54e7af69d858bc5c1a35df52fcffb52
Paolo Fermin <paolofermin@vt.edu>
2020-05-08 12:54:47 -0400

    added more predefined macros

--- main.cpp
+++ main.cpp
@@ -2,8 +2,11 @@
 #include<math.h>
 #include <iostream>
 
-#define pi 3.1416
+#define pi 3.14159265
 #define f 1000.0 // 1000 Hz signal
+#define speedOfSound 343
+#define speakerAngle 45
+#define micLength .1
 #define theta1 90 // phase angle LEFT
 #define theta2 -85	//phase angle RIGHT
 
@@ -24,6 +27,11 @@ float toRadians(int deg) {
 	return (pi / 180) * deg;
 }
 
+float getPhaseDifferenceFromAngle(int angle) {
+	float deltaT = micLength * cos(toRadians(angle)) / speedOfSound;
+	cout << deltaT << endl;
+	return 2 * pi * deltaT * f;
+}
 
 int main(void)
 {
@@ -31,16 +39,19 @@ int main(void)
 	int i, n;
 	FILE* output_file1;
 	FILE* output_file2; // declare a pointer pointing to a file
-	output_file1 = fopen("signal1.txt", "w"); // “w” is the argument for writing
+	output_file1 = fopen("signal1.txt", "w"); // ï¿½wï¿½ is the argument for writing
 	output_file2 = fopen("signal2.txt", "w");
 
 	n = (tf - t0) / step; // samples from 0 to n-1
 	for (i = 0; i < n; i++)
 	{
 		t = t0 + i * step;
-		p1 = 2.5 * sin(2 * pi * f * t - toRadians(theta1)) + 2.5; // samples from 0 to n-1
-		p2 = 2.5 * sin(2 * pi * f * t - toRadians(theta2)) + 2.5; // samples from 0 to n-1
-		
+
+		float phaseDifference = getPhaseDifferenceFromAngle(speakerAngle);
+		cout << phaseDifference << endl;
+		p1 = 2.5 * sin(2 * pi * f * t) + 2.5; // samples from 0 to n-1
+		p2 = 2.5 * sin(2 * pi * f * t - phaseDifference) + 2.5; // samples from 0 to n-1
+
 		fprintf(output_file1, "%d\n", adc(p1)); // write the samples into a file
 		fprintf(output_file2, "%d\n", adc(p2));
 	}
