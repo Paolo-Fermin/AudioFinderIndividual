3c5c84a5258374f26b85c3f4fa742d74192eab3f
Paolo Fermin <paolofermin@vt.edu>
2020-05-08 13:51:13 -0400

    cleaned up and commented for readability

--- displayAngle.py
+++ displayAngle.py
@@ -6,69 +6,47 @@ import matplotlib.pyplot as plt
 import os
 import pdb
 
-
+#define constants
 c = 343.0
 l = .1
 T = 1e-3
 
-left_file = os.path.join(os.getcwd(), 'left_signal.txt')
-right_file = os.path.join(os.getcwd(), 'right_signal.txt')
+#helper function 
+def getAngleFromTime(deltaT):
+    return np.arccos(c * deltaT/l)
 
+#change this value to try different test cases
+#note that this value is NOT the value which is displayed but just to point to the 
+#correct data file
+TEST_CASE = '120'
 
-table = pd.read_csv(left_file)
+#READ DATA
+left_filepath = os.path.join('test_cases', TEST_CASE, 'left_signal.txt')
+right_filepath = os.path.join('test_cases', TEST_CASE, 'right_signal.txt')
+
+table = pd.read_csv(left_filepath)
 table.rename(columns={'signal':'left'}, inplace=True)
-table2 = pd.read_csv(right_file)
-#pdb.set_trace()
+table2 = pd.read_csv(right_filepath)
 
 table = table.join(table2['signal'])
 table.rename(columns={'signal':'right'}, inplace=True)
 
+
+#CALCULATE ANGLE FROM DATA
 left_min_index = table['left'].idxmin()
 right_min_index = table['right'].idxmin()
 
-print(left_min_index)
-print(right_min_index)
-
-
 time_interval = T / len(table.index)
-print("time interval = " + str(time_interval))
 
 left_time = table.loc[left_min_index, 'time']
 right_time = table.loc[right_min_index, 'time']
 deltaT = left_time - right_time
 
-print("left time = " + str(left_time))
-print("right time = " + str(right_time))
-
-left_dist = (left_time - right_time) * c
-right_dist = (right_time - left_time) * c
-
-print("left dist = " + str(left_dist))
-print("right dist = " + str(right_dist))
-
-def getAngleFromTime(deltaT):
-    return np.arccos(c * deltaT/l)
-
-#pdb.set_trace()
-'''
-if (left_dist > right_dist) :
-    distance = (right_time - left_time) * c
-    angle = np.arccos(right_dist/l)
-    angle_degrees = np.degrees(angle)
-else:
-    angle = np.arccos(left_dist/l)
-    angle_degrees = np.degrees(angle)
-'''
-
 angle = getAngleFromTime(deltaT)
 angle_degrees = np.degrees(angle)
 
-#pdb.set_trace()
 
-print("angle = " + str(angle))
-print("angle (degrees) = " + str(angle_degrees))
-
-
+#DISPLAY DATA ON GUI
 win = GraphWin('Speaker Angle', 1000, 750)
 origin = Point(win.getWidth()/2, win.getHeight() - 10)
 origin.draw(win)
@@ -96,10 +74,3 @@ angle_label.draw(win)
 win.getMouse()
 win.close()
 
-'''
-plt.figure()
-plt.plot(table['Left'])
-plt.plot(table['Right'])
-plt.plot(table['Difference'])
-plt.show()
-'''
