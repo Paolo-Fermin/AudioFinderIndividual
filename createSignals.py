d7f73c2fe4d2c7f4c7c99d9238f2c3d4d1d35fca
Paolo Fermin <paolofermin@vt.edu>
2020-05-08 22:38:00 -0400

    cleaned up and commented

--- createSignals.py
+++ createSignals.py
@@ -4,10 +4,15 @@ import pandas as pd
 import pdb
 import os
 
+#define constants
 f = 1000.0
 c = 343
 speaker_angle = 120
 mic_distance = .1
+t0 = 0.0
+tf = 1e-3
+step = 5e-6
+n = (tf - t0) / step
 
 def adc(analog):
     analogHigh = 5
@@ -18,17 +23,12 @@ def getPhaseDiffFromAngle(angle):
     delta_t = mic_distance * np.cos(np.radians(speaker_angle)) / c
     return 2 * math.pi * delta_t * f
 
-t0 = 0.0
-tf = 1e-3
-step = 5e-6
-
-n = (tf - t0) / step
-
 #initialize arrays to store the data
 left_signal = np.zeros(int(n))
 right_signal = np.zeros(int(n))
 time = np.zeros(int(n))
 
+#sample the sinusoid at a certain rate and store the values in an array
 for i in range(int(n)):
     t = t0 + i * step
     time[i] = format(t, '.6g') #limit significant figures
@@ -37,7 +37,7 @@ for i in range(int(n)):
     left_signal[i] = adc(2.5 * np.cos(2*math.pi*f*t - phase_diff/2) + 2.5)
     right_signal[i] = adc(2.5 * np.cos(2*math.pi*f*t + phase_diff/2) + 2.5)
 
-#array now has all sinusoid values in two arrays
+#now all sinusoid values in two arrays
 #write arrays to file
 left_table = pd.DataFrame({'time':time, 'signal':left_signal})
 right_table = pd.DataFrame({'time':time, 'signal':right_signal})
